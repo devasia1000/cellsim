769324782e3ae868acaa960084f80c2403d17a96
Devasia Manuel <devasia1000@gmail.com>
2013-07-20 15:00:16 -0400

    using sprint trace files for cellsim

---
+++ start_cellsim.pl
@@ -0,0 +1,7 @@
+print `make`;
+
+#./cellsim <up> <down> <client_mac> <loss_rate>
+print "Enter Client MAC:\n";
+$mac=<STDIN>;
+chomp($mac);
+print `./cellsim sprint-uplink_processed.txt sprint-downlink_processed.txt ${mac} 0`;
