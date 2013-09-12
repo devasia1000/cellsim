971eaca41a0e47ddfc5a9c038b4def00bfbb4ce5
Devasia Manuel <devasia1000@gmail.com>
2013-09-11 20:51:06 -0500

    cellsim, youtube_playback and chromium play well together

--- cellsim_with_youtube_playback.py
+++ cellsim_with_youtube_playback.py
@@ -20,104 +20,13 @@ from os.path import expanduser
  
 #################################
 
-def startNAT( root, inetIntf='eth0', subnet='10.0/8' ):
-    """Start NAT/forwarding between Mininet and external network
-    root: node to access iptables from
-    inetIntf: interface for internet access
-    subnet: Mininet subnet (default 10.0/8)="""
- 
-    # Identify the interface connecting to the mininet network
-    localIntf =  root.defaultIntf()
- 
-    # Flush any currently active rules
-    root.cmd( 'iptables -F' )
-    root.cmd( 'iptables -t nat -F' )
- 
-    # Create default entries for unmatched traffic
-    root.cmd( 'iptables -P INPUT ACCEPT' )
-    root.cmd( 'iptables -P OUTPUT ACCEPT' )
-    root.cmd( 'iptables -P FORWARD DROP' )
- 
-    # Configure NAT
-    root.cmd( 'iptables -I FORWARD -i', localIntf, '-d', subnet, '-j DROP' )
-    root.cmd( 'iptables -A FORWARD -i', localIntf, '-s', subnet, '-j ACCEPT' )
-    root.cmd( 'iptables -A FORWARD -i', inetIntf, '-d', subnet, '-j ACCEPT' )
-    root.cmd( 'iptables -t nat -A POSTROUTING -o ', inetIntf, '-j MASQUERADE' )
- 
-    # Instruct the kernel to perform forwarding
-    root.cmd( 'sysctl net.ipv4.ip_forward=1' )
- 
-def stopNAT( root ):
-    """Stop NAT/forwarding between Mininet and external network"""
-    # Flush any currently active rules
-    root.cmd( 'iptables -F' )
-    root.cmd( 'iptables -t nat -F' )
- 
-    # Instruct the kernel to stop forwarding
-    root.cmd( 'sysctl net.ipv4.ip_forward=0' )
- 
-def fixNetworkManager( root, intf ):
-    """Prevent network-manager from messing with our interface,
-       by specifying manual configuration in /etc/network/interfaces
-       root: a node in the root namespace (for running commands)
-       intf: interface name"""
-    cfile = '/etc/network/interfaces'
-    line = '\niface %s inet manual\n' % intf
-    config = open( cfile ).read()
-    if ( line ) not in config:
-        print '*** Adding', line.strip(), 'to', cfile
-        with open( cfile, 'a' ) as f:
-            f.write( line )
-    # Probably need to restart network-manager to be safe -
-    # hopefully this won't disconnect you
-    root.cmd( 'service network-manager restart' )
- 
-#For remote playback
-def connectToInternet( network, switch='s1', rootip='10.254', subnet='10.0/8'):
-    """Connect the network to the internet
-       switch: switch to connect to root namespace
-       rootip: address for interface in root namespace
-       subnet: Mininet subnet"""
-    switch = network.get( switch )
-    prefixLen = subnet.split( '/' )[ 1 ]
-    routes = [ subnet ]  # host networks to route to
- 
-    # Create a node in root namespace
-    root = Node( 'root', inNamespace=False )
- 
-    # Prevent network-manager from interfering with our interface
-    fixNetworkManager( root, 'root-eth0' )
- 
-    # Create link between root NS and switch
-    link = network.addLink( root, switch )
-    link.intf1.setIP( rootip, prefixLen )
- 
-    # Start network that now includes link to root namespace
-    network.start()
- 
-    # Start NAT and establish forwarding
-    startNAT( root )
-  
-    # Establish routes from end hosts
-    for host in network.hosts:
-        host.cmd( 'ip route flush root 0/0' )
-        host.cmd( 'route add -net', subnet, 'dev', host.defaultIntf() )
-        host.cmd( 'route add default gw', rootip )
- 
-    return root
-
 def setupRoutes(network):
         
     for host in network.hosts:
 	if host.name is "cellsim":	    
 	    host.cmd("sudo ifconfig cellsim-eth1 10.0.0.5 up")
-	    # delete old routes
-            #host.cmd("sudo route del -net 0.0.0.0 netmask 0.0.0.0 cellsim-eth0")
-            #host.cmd("sudo route del -net 10.0.0.0 netmask 255.0.0.0 cellsim-eth0")
-	    # create new routes
-	    #host.cmd("sudo route add 10.0.0.2 cellsim-eth1")
-	    #host.cmd("sudo route add 10.0.0.3 cellsim-eth0")
-	    
+	   
+ 
 def startCellsimAndYouTubePlayback(network, username, uplink, downlink, lossRate):	
 
     # find the client's MAC address and pass it to cellsimi
@@ -138,7 +47,8 @@ def startCellsimAndYouTubePlayback(network, username, uplink, downlink, lossRate
 
 	elif host.name is "server":
 	    #start youtube_playback on server
-	    host.cmd("nohup sudo java "+expanduser("~")+"/youtube_playback/Main >/tmp/youtube_playback-stdout 2>/tmp/youtube_playback-stderr &");
+		host.cmd("nohup javac "+expanduser("~")+"/youtube_playback/*.java >/tmp/youtube_playback-stdout 2>/tmp/youtube_playback-stderr &");	   	
+		host.cmd("nohup sudo java "+expanduser("~")+"/youtube_playback/Main >/tmp/youtube_playback-stdout 2>/tmp/youtube_playback-stderr &");
 
 
 def startChromium(network, username, videolink):
@@ -200,8 +110,6 @@ if __name__ == '__main__':
     net = CellsimNet()
     net.start()
     net.startTerms()
-    # Configure and start NATted connectivity
-    #rootnode = connectToInternet( net )
     
     print "*** Hosts are running"
     print "*** Type 'exit' or control-D to shut down network"
@@ -209,7 +117,7 @@ if __name__ == '__main__':
     setupRoutes(net)
     # start cellsim
     startCellsimAndYouTubePlayback(net, username, uplink, downlink, lossrate)
-    sleep(10)
+    sleep(5)
     #start chromium
     startChromium(net, username, videolink)
 
